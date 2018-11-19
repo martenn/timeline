b608b1cba3dfde4977e64ff77470437fc5f6f65b
martenn@go2.pl <martenn@go2.pl>
2018-11-19 21:12:29 +0100

    init timeline

---
+++ src/main/java/pl/martenn/timeline/TimelineApplication.java
@@ -0,0 +1,12 @@
+package pl.martenn.timeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelineApplication.class, args);
+	}
+}
